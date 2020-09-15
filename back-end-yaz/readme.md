2b9ef94f8c05075e4ac591c3b1632c639a33437a
jduques <juan.duque.serna@correounivalle.edu.co>
2020-09-15 08:57:02 -0500

    starting express

--- back-end-yaz/readme.md
+++ back-end-yaz/readme.md
@@ -13,3 +13,6 @@ SECRET_KEY = any_secret
 when you download this project, write in the comand line "npm install" to download the node_modules package
 
 to start running the project run : "npm start"
+
+
+note for future me: cors was not save when installed
--- back-end-yaz/src/index.js
+++ back-end-yaz/src/index.js
@@ -1,6 +1,22 @@
 import 'dotenv/config';
+import cors from 'cors';
+import express from 'express';
+ 
+const app = express();
 
-console.log('Hello Project.');
+app.use(cors());
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+  });
+
+  app.get('*', (req, res) => {
+    res.send('URL not found!');
+  });
+
+app.listen(process.env.PORT, () =>
+  console.log('Example app listening on port ', process.env.PORT),
+);
 
 console.log(process.env.HOST);
 console.log(process.env.PORT);
