fcdcc6c25f588b0f958f8fb0185f47e65adbd6e7
Angelica <aagudelo@truora.com>
2020-09-19 16:57:12 -0500

    Add payment service model
    What: Add payment service model
    
    Why: We are implement the ORM
    
    YAZ-008

---
+++ back-end-yaz/src/models/payment_service.js
@@ -0,0 +1,22 @@
+const payment_service = (sequelize, DataTypes) => {
+    const Payment_Service = sequelize.define('payment_service', {
+      id_payment_service: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+        allowNull: false,
+      },
+      id_service: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      payment_date: {
+        type: DataTypes.DATE,
+        allowNull: false,
+      },
+    });
+   
+    return Payment_Service;
+};
+
+export default payment_service;
