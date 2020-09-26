3f2720f443354d266ac43bd0a4e0dbd62ada6f2e
jduques <juan.duque.serna@correounivalle.edu.co>
2020-09-26 14:54:27 -0500

    paymet_items error handler

--- back-end-yaz/src/server/controllers/Payment_itemsController.js
+++ back-end-yaz/src/server/controllers/Payment_itemsController.js
@@ -6,48 +6,72 @@ const util = new Util();
 class Payment_itemsController {
 
     static async getAllPayment_items(req, res) {
-        util.setSuccess(200, `All payment_items returned!`);
-        return util.send(res);
+        try {
+            util.setSuccess(200, `All payment_items returned!`);
+            return util.send(res);            
+        } catch (error) {
+            util.setError(400, error);
+            return util.send(res);
+        }
     }
 
     static async addPayment_items(req, res) {
-        util.setSuccess(200, `New payment_items created!`);
-        return util.send(res);
+        try {
+            util.setSuccess(200, `New payment_items created!`);
+            return util.send(res);
+        } catch (error) {
+            util.setError(400, error);
+            return util.send(res);
+        };
     }
 
     static async updatePayment_itemsById(req, res) {
         const { id } = req.params;
-        if (!Number(id)) {
-            util.setError(400, 'Please input a valid numeric value');
+        try {
+            if (!Number(id)) {
+                util.setError(400, 'Please input a valid numeric value');
+                return util.send(res);
+            } else {
+                util.setSuccess(200, `Payment_items ${id} updated!`);
+                return util.send(res);
+            }
+        } catch (error) {
+            util.setError(400, error);
             return util.send(res);
-        } else {
-            util.setSuccess(200, `Payment_items ${id} updated!`);
-            return util.send(res);
-        }
+        }       
     }
 
     static async getPayment_itemsById(req, res) {
         const { id } = req.params;
-
-        if (!Number(id)) {
-            util.setError(400, 'Please input a valid numeric value');
+        try {
+            if (!Number(id)) {
+                util.setError(400, 'Please input a valid numeric value');
+                return util.send(res);
+            } else {
+                util.setSuccess(200, `Payment_items ${id} returned!`);
+                return util.send(res);
+            }
+        } catch (error) {
+            util.setError(400, error);
             return util.send(res);
-        } else {
-            util.setSuccess(200, `Payment_items ${id} returned!`);
-            return util.send(res);
-        }
+        }        
     }
 
     static async deletePayment_itemsById(req, res) {
         const { id } = req.params;
-        if (!Number(id)) {
-            util.setError(400, 'Please provide a numeric value');
-            return util.send(res);
-        } else if (req.body.user_type > 0) {
-            util.setError(400, `You do not have permission to do this!`);
-            return util.send(res);
-        } else {
-            util.setSuccess(200, `User ${req.body.name} deleted Payment_items ${id}!`);
+        try {
+            if (!Number(id)) {
+                util.setError(400, 'Please provide a numeric value');
+                return util.send(res);
+            } else if (req.body.user_type > 0) {
+                util.setError(400, `You do not have permission to do this!`);
+                return util.send(res);
+            } else {
+                util.setSuccess(200, `User ${req.body.name} deleted Payment_items ${id}!`);
+                return util.send(res);
+            }            
+        } catch (error) {
+            util.setError(400, error);
             return util.send(res);
         }
     }
