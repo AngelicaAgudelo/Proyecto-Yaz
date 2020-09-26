71d099380eee9c3c4420d2f922db7888ab949b27
jduques <juan.duque.serna@correounivalle.edu.co>
2020-09-26 16:33:43 -0500

    implementing services for item

--- back-end-yaz/src/server/controllers/ItemController.js
+++ back-end-yaz/src/server/controllers/ItemController.js
@@ -1,4 +1,4 @@
-//import ItemService from '../services/ItemService';
+import ItemService from '../services/ItemService';
 import Util from '../utils/Utils';
 
 const util = new Util();
@@ -7,7 +7,12 @@ class ItemController {
 
     static async getAllItems(req, res) {
         try {
-            util.setSuccess(200, `All items returned!`);
+            const allItems = await ItemService.getAllItems();
+            if(allItems.length > 0){
+                util.setSuccess(200, 'Items returned', allItems)
+            } else {
+                util.setSuccess(500, 'No items found');
+            }
             return util.send(res);            
         } catch (error) {
             util.setError(400, error);
@@ -16,9 +21,11 @@ class ItemController {
     }
 
     static async addItem(req, res) {
+        const newItem = req.body
         try {
-            util.setSuccess(200, `New item created!`);
-            return util.send(res);            
+            const createdItem = await ItemService.addItem(newItem);
+            util.setSuccess(201, `New item created!`, createdItem);
+            return util.send(res);
         } catch (error) {
             util.setError(400, error);
             return util.send(res);
@@ -27,14 +34,17 @@ class ItemController {
 
     static async updateItemById(req, res) {
         const { id } = req.params;
+        const alteredItem = req.body;
         try {
+            const updateUser = await ItemService.updateItemById(id, alteredItem)
             if (!Number(id)) {
                 util.setError(400, 'Please input a valid numeric value');
-                return util.send(res);
-            } else {
+            } else if(updateUser){
                 util.setSuccess(200, `Item ${id} updated!`);
-                return util.send(res);
+            }else{
+                util.setError(400, `Could not update item ${id}!`)
             }
+            return util.send(res);
         } catch (error) {
             util.setError(400, error);
             return util.send(res);
@@ -44,13 +54,15 @@ class ItemController {
     static async getItemById(req, res) {
         const { id } = req.params;
         try {
+            const theItem = await ItemService.getItemById(id);
             if (!Number(id)) {
                 util.setError(400, 'Please input a valid numeric value');
-                return util.send(res);
-            } else {
+            } else if(theItem){
                 util.setSuccess(200, `Item ${id} returned!`);
-                return util.send(res);
+            }else{
+                util.setSuccess(400, `Could not found item ${id}!`);
             }
+            return util.send(res);
         } catch (error) {
             util.setError(400, error);
             return util.send(res);
@@ -60,16 +72,17 @@ class ItemController {
     static async deleteItemById(req, res) {
         const { id } = req.params;
         try {
+            const itemToDelete = await ItemService.deleteItemById(id);
             if (!Number(id)) {
                 util.setError(400, 'Please provide a numeric value');
-                return util.send(res);
             } else if (req.body.user_type > 0) {
                 util.setError(400, `You do not have permission to do this!`);
-                return util.send(res);
-            } else {
+            } else if(itemToDelete){
                 util.setSuccess(200, `User ${req.body.name} deleted Item ${id}!`);
-                return util.send(res);
-            }            
+            }else{
+                util.setError(400, `User with the id ${id} cannot be found`);
+            }
+            return util.send(res);
         } catch (error) {
             util.setError(400, error);
             return util.send(res);
---
+++ back-end-yaz/src/server/services/ItemService.js
@@ -0,0 +1,66 @@
+import database from '../src/models';
+
+class ItemService {
+  static async getAllItems() {
+    try {
+      return await database.Item.findAll();
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  static async addItem(newItem) {
+    try {
+      return await database.Item.create(newItem);
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  static async updateItemById(id, updateItem) {
+    try {
+      const itemToUpdate = await database.Item.findOne({
+        where: { id: Number(id) }
+      });
+
+      if (itemToUpdate) {
+        await database.Item.update(updateItem, { where: { id: Number(id) } });
+
+        return updateItem;
+      }
+      return null;
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  static async getItemById(id) {
+    try {
+      const theItem = await database.Item.findOne({
+        where: { id: Number(id) }
+      });
+
+      return theItem;
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  static async deleteItemById(id) {
+    try {
+      const itemToDelete = await database.Item.findOne({ where: { id: Number(id) } });
+
+      if (itemToDelete) {
+        const deletedItem = await database.Item.destroy({
+          where: { id: Number(id) }
+        });
+        return deletedItem;
+      }
+      return null;
+    } catch (error) {
+      throw error;
+    }
+  }
+}
+
+export default ItemService;
