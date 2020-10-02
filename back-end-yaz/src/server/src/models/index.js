91f8f414a5ff1628d265f183af6f9efbacddeac9
jduques <juan.duque.serna@correounivalle.edu.co>
2020-10-01 20:04:09 -0500

    adding things

--- back-end-yaz/src/server/src/models/index.js
+++ back-end-yaz/src/server/src/models/index.js
@@ -1,7 +1,7 @@
 import Sequelize from 'sequelize';
 
 const sequelize = new Sequelize(
-    configprocess.env.DB_NAME,
+    process.env.DB_NAME,
     process.env.DB_USER,
     process.env.DB_PASS,
     {
