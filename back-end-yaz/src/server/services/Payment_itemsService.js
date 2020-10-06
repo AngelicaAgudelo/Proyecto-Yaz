c0e5e29d98a99c367366c52b4d89acc3f1373aab
jduques <juan.duque.serna@correounivalle.edu.co>
2020-10-06 18:42:32 -0500

    Fixed endpoints for table payment_service

--- back-end-yaz/src/server/services/Payment_itemsService.js
+++ back-end-yaz/src/server/services/Payment_itemsService.js
@@ -3,7 +3,7 @@ import models from '../src/models';
 class Payment_itemsService {
   static async getAllPayment_itemss() {
     try {
-      return await models.Payment_items.findAll();
+      return await models.payment_items.findAll();
     } catch (error) {
       throw error;
     }
@@ -11,7 +11,7 @@ class Payment_itemsService {
 
   static async addPayment_items(newPayment_items) {
     try {
-      return await models.Payment_items.create(newPayment_items);
+      return await models.payment_items.create(newPayment_items);
     } catch (error) {
       throw error;
     }
@@ -19,12 +19,12 @@ class Payment_itemsService {
 
   static async updatePayment_itemsById(id, updatePayment_items) {
     try {
-      const payment_itemsToUpdate = await models.Payment_items.findOne({
+      const payment_itemsToUpdate = await models.payment_items.findOne({
         where: { id_payment_item: Number(id) }
       });
 
       if (payment_itemsToUpdate) {
-        await models.Payment_items.update(updatePayment_items, { where: { id_payment_item: Number(id) } });
+        await models.payment_items.update(updatePayment_items, { where: { id_payment_item: Number(id) } });
 
         return updatePayment_items;
       }
@@ -36,7 +36,7 @@ class Payment_itemsService {
 
   static async getPayment_itemsById(id) {
     try {
-      const returnedPayment_items = await models.Payment_items.findOne({
+      const returnedPayment_items = await models.payment_items.findOne({
         where: { id_payment_item: Number(id) }
       });
 
@@ -48,10 +48,10 @@ class Payment_itemsService {
 
   static async deletePayment_itemsById(id) {
     try {
-      const payment_itemsToDelete = await models.Payment_items.findOne({ where: { id_payment_item: Number(id) } });
+      const payment_itemsToDelete = await models.payment_items.findOne({ where: { id_payment_item: Number(id) } });
 
       if (payment_itemsToDelete) {
-        const deletedPayment_items = await models.Payment_items.destroy({
+        const deletedPayment_items = await models.payment_items.destroy({
           where: { id_payment_item: Number(id) }
         });
         return deletedPayment_items;
