70092d55281405f2b26731178b6e76160551009a
jduques <juan.duque.serna@correounivalle.edu.co>
2020-10-17 15:41:07 -0500

    creating another test

--- back-end-yaz/test/test.js
+++ back-end-yaz/test/test.js
@@ -1,15 +1,33 @@
-var expect  = require("chai").expect;
-var request = require("request");
+const chai = require("chai");
+const expect = require("chai").expect;
+const chaiHttp = require('chai-http');
+const request = require("request");
 
-describe("Yaz API", function() {
-  describe("Get all users endpoints", function() {
-    var url = "http://localhost:3000/users";
+chai.use(chaiHttp);
+const url = "http://localhost:3000";
 
-    it("returns status 200", function(done) {
-      request(url, function(error, response, body) {
-        expect(response.statusCode).to.equal(200);
-        done();
-      });
+describe("Yaz API", function () {
+  describe("Get all users endpoints", function () {
+    it("returns status 200", function (done) {
+      chai.request(url)
+        .get('/users')
+        .end(function (err, res) {
+          expect(res).to.have.status(200);
+          done();
+        });
+    });
+  });
+
+  describe('Insert a user', function () {
+    it('should insert a user', function (done) {
+      chai.request(url)
+        .post('/users')
+        .send({ user_name: "Jhon Doe", user_type: 0, user_password: "jhon", user_email: "jhon@doe.com" })
+        .end(function (err, res) {
+          console.log(res.body)
+          expect(res).to.have.status(201);
+          done();
+        });
     });
   });
 });
